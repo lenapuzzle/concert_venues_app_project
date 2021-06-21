06c073713373ac0eef655525c31c0b85838c5903
jessica_mach <Jessica.Mach@outlook.com>
2021-06-21 12:35:07 -0700

    Removed drinks from home controller

--- src/main/java/com/launchacademy/reviews/controllers/HomeController.java
+++ src/main/java/com/launchacademy/reviews/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  @GetMapping(value = {"/drinks"})
+  @GetMapping(value = {"/"})
   public String forward() {
     return "forward:/";
   }
