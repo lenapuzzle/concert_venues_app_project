aa3c0e0048bb00111ecc270d4c94e833d3ffc5ac
Davon Belvin <davonbelvin@gmail.com>
2021-06-22 11:18:23 -0700

    fixed spacing issues and refactored seeder/mirgation

--- src/main/java/com/launchacademy/reviews/controllers/VenueController.java
+++ src/main/java/com/launchacademy/reviews/controllers/VenueController.java
@@ -4,8 +4,13 @@ import com.launchacademy.reviews.models.Venue;
 import com.launchacademy.reviews.services.VenueService;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +30,17 @@ private VenueService venueService;
     venuesMap.put("venues", venueService.findAllVenues());
     return venuesMap;
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Map<String,Venue>> getVenueById(@PathVariable Integer id){
+    Map<String, Venue> requestedVenue = new HashMap<>();
+    Optional<Venue> queriedVenue = venueService.findById(id);
+    if(queriedVenue.isPresent()){
+      requestedVenue.put("venue", queriedVenue.get());
+      return new ResponseEntity<Map<String,Venue>>(requestedVenue, HttpStatus.OK);
+    }else{
+      return new ResponseEntity<Map<String,Venue>>(requestedVenue, HttpStatus.NOT_FOUND);
+    }
+  }
+
 }
--- src/main/java/com/launchacademy/reviews/services/VenueService.java
+++ src/main/java/com/launchacademy/reviews/services/VenueService.java
@@ -6,21 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VenueService {
-private VenueRepository venueRepository;
+  private VenueRepository venueRepository;
 
   @Autowired
   public VenueService( VenueRepository venueRepository) {
     this.venueRepository = venueRepository;
   }
 
-public List<Venue> findAllVenues(){
+  public List<Venue> findAllVenues(){
     return (List<Venue>) venueRepository.findAll();
-}
+  }
 
-public Venue save(Venue venue) {
+  public Venue save(Venue venue) {
      return venueRepository.save(venue);
-    }
+  }
+
+  public Optional<Venue> findById(Integer id) {
+      return venueRepository.findById(id);
+  }
+
+  public long count() {
+    return venueRepository.count();
+  }
 }
