e03c977d7e031eaf7a998fd232bd6aff19a03431
Ash Manning <ashleylmanning@gmail.com>
2021-06-28 13:33:49 -0400

    added more seeder data

--- src/main/java/com/launchacademy/reviews/seeders/ReviewSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/ReviewSeeder.java
@@ -19,7 +19,7 @@ public class ReviewSeeder {
   }
 
   public void run(String... args) throws Exception {
-    if (reviewService.count() <= 0){
+    if (reviewService.count() <= 0) {
       Venue elCorazon = venueService.findByName("El Corazon");
       Review review1 = new Review();
       review1.setEventName("Moby");
@@ -53,7 +53,105 @@ public class ReviewSeeder {
       review4.setRating(8);
       review4.setVenue(soDo);
       reviewService.save(review4);
-    }
 
-  }
-}
+      Venue thaliaHall = venueService.findByName("Thalia Hall");
+      Review review5 = new Review();
+      review5.setEventName("Phil Lesh and the Terrapin Family Band");
+      review5.setUserName("DeadHead5");
+      review5.setText(
+          "Last week was my second time at Thalia Hall and I'm glad Chicago has a venue like this.\n"
+              + "\n"
+              + "I came to see Phil Lesh and the Terrapin Family Band, which was awesome to be able to see member of the Grateful Dead in a smaller venue.\n"
+              + "\n"
+              + " Be prepared to stay in a line and if dress warm. Chicago weather can be brutal. Security made sure to keep people moving so everyone could get out of the cold.\n"
+              + "\n"
+              + "It's a big open floor with a small mezzanine with seats. Good amount of bars on the first floor. Thankfully the bar lights/crowd doesn't interfere with the show. \n"
+              + "\n"
+              + "The acoustics were great. The venue was rehabed a few years ago and they did a great job of keeping original aesthetic while modernizing the sound. I read it was originally an opera house, it's awesome that the stayed true the venue. \n"
+              + "\n"
+              + "Thalia Hall is one of the best venues in Chicago!");
+      review5.setRating(10);
+      review5.setVenue(thaliaHall);
+      reviewService.save(review5);
+
+      Venue greatAmericanMusicHall = venueService.findByName("Great American Music Hall");
+      Review review6 = new Review();
+      review6.setEventName("The 70s");
+      review6.setUserName("SanFranMan");
+      review6.setText("This was my favorite venue to go to in the 70's when they booked jazz.\n"
+          + " I saw a lot of greate concerts here., when I was younger\n"
+          + "I sat pretty much everywhere, upstairs in the balcony and on the ground floor. It used to cost a dollar for most tickets!Some of the best shows I saw were Sarah Vaughan, Joe Williams, Toots Thielmanns, Dizzy Gillespie, Chick Corea, the Count Basie Band and others lost in the mists of time. "
+          + " It was a great venue back in the day. I haven't been there for years but its still around an kicking. It was a nice sized venue w/ great sound, between the small clubs like Keystone Corner and the large venues like the Masonic Auditorium.  I have greate  memories of evenings spent here with friends listening to great music.  A staple of San Fran that I hope stays forever");
+      review6.setRating(9);
+      review6.setVenue(greatAmericanMusicHall);
+      reviewService.save(review6);
+
+      Venue artspace = venueService.findByName("3S Artspace");
+      Review review7 = new Review();
+      review7.setEventName("Animal Collective");
+      review7.setUserName("Dirtywater");
+      review7.setText(
+          "This place is amazing....Drove here from Boston to check out the show last night. Great Venue, love the taco bar out front.  Nice small town vibe. I hope to get to see another show soon.\n"
+              + "\n"
+      );
+      review7.setRating(9);
+      review7.setVenue(artspace);
+      reviewService.save(review7);
+
+      Venue theSinclair = venueService.findByName("The Sinclair");
+      Review review8 = new Review();
+      review8.setEventName("Run the Jewels");
+      review8.setUserName("musicfan");
+      review8.setText(
+          " The Sinclair is one of the best venues in Boston. I love the small club vibe and the fact that there's great view of the stage no matter where you stand.  That being said make sure you get there early, it was a bit crowded at sold out shows "
+              + "\n"
+              + "Parking can be a bit tough due to the level of activity in that area of Cambridge. There is a pay lot down the street but its small and quite expensive.\n"
+              + "\n"
+      );
+      review8.setRating(9);
+      review8.setVenue(theSinclair);
+      reviewService.save(review8);
+
+      Venue theRoyale = venueService.findByName("The Royale");
+      Review review9 = new Review();
+      review9.setEventName("Kamasi Washington");
+      review9.setUserName("JazzzzzMannnnn");
+      review9
+          .setText(" This is a decent concert venue, if there's an act you really want to see.\n "
+              + "Turns into a club after concerts so shows end early and they tend to rush you out.\n"
+              + " Sound is hit or miss. Really depends on if the band's engineer knows how to work with the space. From what I witness they the venue doesn't see like much help. "
+              + "Make sure to get there early the front of the floor is on a platform (for the club dance floor). If you don't get there early you'll get stuck on the lower floor and its hard to see the stage unless you're very tall. \n"
+          );
+      review9.setRating(9);
+      review9.setVenue(theRoyale
+      );
+      reviewService.save(review9);
+
+      Review review10 = new Review();
+      review10.setEventName("Grateful Dead");
+      review10.setUserName("MayerisnoGarcia");
+      review10.setText(
+          "I saw the dead here on August 13, 1975.  It was the first time Blues for Allah was performed live in its entirety as the the show wasbroadcast on radio stations across the country to promote the album. There's something about the performance of 'Franklin's Tower' that has never left me considering its a pretty straight forward performance.  ");
+      review10.setRating(10);
+      review10.setVenue(greatAmericanMusicHall);
+      reviewService.save(review10);
+
+      Review review11 = new Review();
+      review11.setEventName("Four Tet");
+      review11.setUserName("FreemontTroll");
+      review11.setText("I like this venue. Its smaller than some others downtown and you can get close to the stage if you arrive early enough. It's small enough that you pretty much you always have a good view. There's a bar with seats if you don't want to stand and there is also a bar on the main floor. I wish the beer selection was a better. Mix drinks are strong :)");
+      review11.setRating(8);
+      review11.setVenue(soDo);
+      reviewService.save(review11);
+
+      Venue wangCenter = venueService.findByName("Boch Center - Wang Theatre");
+      Review review12 = new Review();
+      review12.setEventName("Neil Young Solo");
+      review12.setUserName("Downbytheriver");
+      review12.setText("Neil was on fire last night. Keep on Rockin' in the Free World!");
+      review12.setRating(10);
+      review12.setVenue(wangCenter);
+      reviewService.save(review12);
+    }
+    }
+ }
--- src/main/java/com/launchacademy/reviews/seeders/VenueSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/VenueSeeder.java
@@ -43,6 +43,105 @@ public class VenueSeeder {
       venue2.setImgUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJsX5K_qncFXLDr6hHjs3H7xoJcYOoIwvUew&usqp=CAU");
 
       venueService.save(venue2);
+
+      Venue venue3 = new Venue();
+      venue3.setName("The Royale");
+      venue3.setCity("Boston");
+      venue3.setState("MA");
+      venue3.setAddress("279 Tremont St");
+      venue3.setZipCode("02116");
+      venue3.setPhoneNumber("617-338-7699");
+      venue3.setCapacity(1200);
+      venue3.setDescription("Spacious multilevel megaclub draws trendy crowds for DJs, dancing & live-music shows.");
+
+      venue3.setImgUrl("https://cdn.wedding-spot.com/__sized__/images/venues/3956/Royale-Boston-MA-99fe4157-686d-41a1-a5e4-1fcf173da8e8-97450e389c42885476f1fbe9bc5bca5a.jpg");
+
+      venueService.save(venue3);
+
+
+      Venue venue4 = new Venue();
+      venue4.setName("The Sinclair");
+      venue4.setCity("Cambridge");
+      venue4.setState("MA");
+      venue4.setAddress("52 Church Street");
+      venue4.setZipCode("02138");
+      venue4.setPhoneNumber("617-547-5200");
+      venue4.setCapacity(525);
+      venue4.setDescription("This gastropub & music venue features New American fare and live acts, with an open kitchen & patio..");
+      venue4.setImgUrl("https://64.media.tumblr.com/6ddda14544cb4e2bfa587357fccdaa82/4a2265bbab98e629-b5/s400x600/7056fcf7a1b82a90b26e99c8e79bdcdd1fd7dc04.jpg");
+
+      venueService.save(venue4);
+
+
+      Venue venue5 = new Venue();
+      venue5.setName("3S Artspace");
+      venue5.setCity("Portsmouth");
+      venue5.setState("NH");
+      venue5.setAddress("319 Vaughan St");
+      venue5.setZipCode("03801");
+      venue5.setPhoneNumber("603-766-3330");
+      venue5.setCapacity(400);
+      venue5.setDescription("3S is committed to presenting a wide range of music, with a focus on adventurous, underrepresented, and unconventional acts. Our aim is to showcase some of the best national and international talent, and shine the spotlight on some of the best talent in the region.");
+      venue5.setImgUrl("https://s3.amazonaws.com/media.wbur.org/wordpress/18/files/2015/09/pic3SDavidJMurrayCleareyephotocom-540x360.jpg");
+
+      venueService.save(venue5);
+
+
+      Venue venue6 = new Venue();
+      venue6.setName("Great American Music Hall");
+      venue6.setCity("San Francisco");
+      venue6.setState("CA");
+      venue6.setAddress("859 O'Farrell S");
+      venue6.setZipCode("94109");
+      venue6.setPhoneNumber("415-885-0750");
+      venue6.setCapacity(600);
+      venue6.setDescription("The Great American Music Hall is a concert hall in San Francisco, California. It is located on O'Farrell Street in the Tenderloin neighborhood on the same block as the Mitchell Brothers O'Farrell Theatre.");
+      venue6.setImgUrl("https://upload.wikimedia.org/wikipedia/commons/6/6a/Great_American_Music_Hall_%28April_1976%29.jpg");
+
+      venueService.save(venue6);
+
+
+
+      Venue venue7 = new Venue();
+      venue7.setName("Hampton Beach Casino Ballroom");
+      venue7.setCity("Hampton");
+      venue7.setState("NH");
+      venue7.setAddress(" 169 Ocean Blvd");
+      venue7.setZipCode("03842");
+      venue7.setPhoneNumber("603-929-4100");
+      venue7.setCapacity(2200);
+      venue7.setDescription("The Hampton Beach Casino Ballroom is a seasonal live music and comedy venue located on the boardwalk of Hampton Beach, New Hampshire, United States. The venue is open from April to November, and within those eight months schedules upwards of 70 shows.");
+      venue7.setImgUrl("https://www.gannett-cdn.com/presto/2020/11/23/NPOH/d443c490-6c73-4e11-9342-5ad624543801-18April-HB-Casino-COVIDsign.jpg?crop=2229,1672,x145,y0&quality=50&width=640");
+
+      venueService.save(venue7);
+
+      Venue venue8 = new Venue();
+      venue8.setName("Thalia Hall");
+      venue8.setCity("Chicago");
+      venue8.setState("IL");
+      venue8.setAddress("1807 S. Allport St.");
+      venue8.setZipCode("60608");
+      venue8.setPhoneNumber("312-526-3851");
+      venue8.setCapacity(800);
+      venue8.setDescription("This landmark venue, designed as an opera house in 1892, hosts concerts by well-known musical acts.");
+      venue8.setImgUrl("https://columbiachronicle.com/wp-content/uploads/2014/02/f1d6bc124037959f0aa5e7a07ba56b6f.jpg");
+
+      venueService.save(venue8);
+
+
+      Venue venue9 = new Venue();
+      venue9.setName("Boch Center - Wang Theatre");
+      venue9.setCity("Boston");
+      venue9.setState("MA");
+      venue9.setAddress("270 Tremont St");
+      venue9.setZipCode("02116");
+      venue9.setPhoneNumber("617-482-9393");
+      venue9.setCapacity(3500);
+      venue9.setDescription("The Wang Theatre is a theatre in Boston. It originally opened in 1925 as the Metropolitan Theatre and was later renamed the Music Hall. It was designed by Clarence Blackall and is located at 252–272 Tremont Street in the Boston Theatre District. The theatre is operated as part of the Boch Center.");
+      venue9.setImgUrl("https://celebrity-series.s3.amazonaws.com/files/venues/wide-med-534a0801.jpg");
+
+      venueService.save(venue9);
+
     }
   }
 }
