a260be6923f4469e07c703509063a620fae89590
lenapuzzle <lenapuzzle@yahoo.com>
2021-06-29 13:18:34 -0700

    changed/refactored code per request

--- src/main/frontend/components/NavBar.js
+++ src/main/frontend/components/NavBar.js
@@ -20,29 +20,23 @@ const fetchConcertVenues = async () => {
       const error = new Error(errorMessage);
       throw error;
     }
-    const venueData = await response.json();
-    setVenue(venueData.venue);
   } catch (error) {
     console.error(`There was an error in fetch: ${error}`);
   }
 }
 
- useEffect(() => {
-   fetchConcertVenues()
- },[venueName])
-
   return (
     <div className="row column">
-    <div className="navbar">
-    </div>
-    <nav>
-        <Link to="/">Home</Link>|
-        <Link to="/concert-venues">Venues</Link>|
-        <Link to="/concert-venues/new">Add Venue</Link>
-      </nav>
       <div className="navbar">
-      
-    </div>
+        </div>
+          <nav>
+            <Link to="/">Home</Link>|
+            <Link to="/concert-venues">Venues</Link>|
+            <Link to="/concert-venues/new">Add Venue</Link>
+          </nav>
+        <div className="navbar">
+      </div>
+     
       <Switch>
         <Route exact path="/">
           <Redirect to="/concert-venues" />
@@ -53,6 +47,7 @@ const fetchConcertVenues = async () => {
         <Route exact path="/admin/:id" component={EditVenue} />
         <Route exact path="/concert-venues/:id" component={VenueShow} />
       </Switch>
+  
     </div>
   )
 }
